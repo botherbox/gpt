5feacc7a06f4f2cb5f5253496c8beb66ca5cee90
songzheng <zheng.song@niu.com>
2023-03-20 15:58:28 +0800

    2

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ set -e
 
 # 构建
 npm run build
+echo '[🎉 BUILD DONE]'
 
 # 进入构建文件夹
 cd dist
@@ -12,15 +13,19 @@ cd dist
 # 如果你要部署到自定义域名
 # echo 'www.example.com' > CNAME
 
+echo '[🎉 GIT Commit...]'
 # git init
 # git checkout -b main
-git add -A
+git add .
 git commit -m 'deploy'
+echo '[🎉 GIT Commit DONE]'
 
 # 如果你要部署在 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # 如果你要部署在 https://<USERNAME>.github.io/<REPO>
+
 git push -f git@github.com:botherbox/gpt.git main:gh-pages
+echo '[🎉 GIT Push DONE]'
 
 cd -
